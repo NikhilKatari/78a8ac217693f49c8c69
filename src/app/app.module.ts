c6e4cc631f9e33823f101d3dec0c78b9eb4755bc
Nikhil Pavan Kumar Katari <nk00684@corp.ojas-it.com>
2021-01-19 09:41:43 +0530

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
+// import {  } from 'ngx-bootstrap/modal';
+import { BsModalService, ModalModule,BsModalRef } from 'ngx-bootstrap/modal';
+
+// import { HttpClientModule } from '@angular/common/http';
+import { ServiceService } from './service.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TableComponent } from './table/table.component';
+import { HttpClient ,HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ModalModule.forRoot(),
+    BrowserAnimationsModule
+
+    // HttpClient
   ],
-  providers: [],
+  providers: [ServiceService,BsModalRef,BsModalService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,40 @@
+import { ServiceService } from './../service.service';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { debug } from 'console';
+// import { BsModalRef } from "ngx-bootstrap/modal";
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+  getData: any;
+  getFullData: any;
+  dataid:any;
+  data: any;
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService, private serviceService: ServiceService) {}
+
+  ngOnInit(): void {
+    this.getTable();
+  }
+  getTable(){
+    this.serviceService.getTableData().subscribe(resp => {
+      this.getData = resp;
+      this.getFullData = this.getData.hits;
+      console.log(this.getFullData);
+    });
+}
+openModal(template: TemplateRef<any>,dataId: number) {
+  debugger;
+  this.dataid = dataId;
+  this.modalRef = this.modalService.show(template,this.dataid);
+  this.getFullData.filter((value,index)=>{
+    return value.dataId == this.getFullData.ObjectId;
+    console.log()
+  });
+
+}
+}
